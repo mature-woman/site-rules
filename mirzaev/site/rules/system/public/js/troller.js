05576b1041bbedc5c93543abd5ecb8ba27654373
mirzaev <arsen@mirzaev.sexy>
2023-03-10 18:25:15 +1000

    ссылка на кошке + рандомный танец + остановка инстасамки при скримере

--- mirzaev/site/rules/system/public/js/troller.js
+++ mirzaev/site/rules/system/public/js/troller.js
@@ -1,65 +1,108 @@
 "use strict";
 
 class troller {
-  static what = {
-    enable() {
-      document.body.onmouseleave = function () {
-        // if (Math.random() > 0.90) {
-        // 10%
+  constructor() {
+    this.what = {
+      // HTML-элемент с изображением
+      sound: document.getElementById("troller_sound_what"),
 
-        troller.what.start();
-        // }
-      };
+      // HTML-элемент со звуком
+      image: document.getElementById("troller_image_what"),
 
-      document.body.onmouseenter = function () {
-        troller.what.end();
-      };
-    },
-    disable() {
-      document.body.onmouseleave = document.body.onmouseenter = undefined;
-    },
-    start() {
-      // Отображение изображения
-      document.getElementById("troller_image_what").classList.add("active");
-
-        // Проигрывание звука
-        troller.what.play(); 
-    },
-    end() {
-      // Сокрытие изображения
-      document.getElementById("troller_image_what").classList.remove("active");
-
-      // Остановка звука
-      document.getElementById("troller_sound_what").pause();
-    },
-    single(event = "onmouseleave") {
-      if (typeof event === "string") {
-        // Получены обязательные входные параметры
-
+      enable() {
+        document.body.onmouseleave = () => this.start();
+        document.body.onmouseenter = () => this.end();
+      },
+      disable() {
+        document.body.onmouseleave = document.body.onmouseenter = undefined;
+      },
+      start() {
         // Отображение изображения
-        document.getElementById("troller_image_what").classList.add("active");
+        this.image.classList.add("active");
 
         // Проигрывание звука
-        troller.what.play(); 
+        this.play();
 
-        document.body[event] = function () {
-          troller.what.end();
+        // Остановка воспроизведения инстасамки
+        if (this.core.instasamka.initialized) this.core.instasamka.pause(); 
+      },
+      end() {
+        // Сокрытие изображения
+        this.image.classList.remove("active");
 
-          document.body[event] = undefined;
-        };
+        // Возобновление воспроизведения инстасамки
+        if (this.core.instasamka.initialized) this.core.instasamka.play();
+
+        // Остановка звука
+        this.sound.pause();
+      },
+      single(event = "onmouseleave") {
+        if (typeof event === "string") {
+          // Получены обязательные входные параметры
+
+          // Отображение изображения
+          this.image.classList.add("active");
+
+          // Проигрывание звука
+          this.play(); 
+
+          document.body[event] = function () {
+            this.end();
+
+            document.body[event] = undefined;
+          };
+        }
+      },
+      play() {
+        // Воспроизведение звука
+        this.sound.currentTime = 0;
+        this.sound.play();
       }
-    },
-    play() {
-      // Инициализация элемента со звуком
-      const what_sound = document.getElementById("troller_sound_what");
+    };
 
-      // Воспроизведение звука
-      what_sound.currentTime = 0;
-      what_sound.play();
-    }
-  };
+    this.instasamka = {
+      // HTML-элемент с аудиозаписью
+      sound: document.getElementById("troller_sound_instasamka"),
 
-  static vk() {
+      initialized: false,
+
+      play(time) {
+        if (this.sound instanceof HTMLElement) {
+          // Найден звук
+         
+          // Инициализация инстасамки
+          if (!this.initialized) this.initialized = true;
+
+          // Инициализация временной метки для воспроизведения 
+          if (typeof time === 'number') this.sound.currentTime = time;
+
+          // Воспроизведение звука
+          this.sound.play();
+        } else {
+          // Не найден звук (подразумевается)
+          
+          console.log('[mirzaev] [troller] Не удалось найти песню инстасамки');
+        }
+      },
+      pause() {
+        if (this.sound instanceof HTMLElement) {
+          // Найден звук
+
+          // Остановка воспроизведения звука
+          this.sound.pause();
+        } else {
+          // Не найден звук (подразумевается)
+          
+          console.log('[mirzaev] [troller] Не удалось найти песню инстасамки');
+        }
+      },
+    };
+
+    // Инициализация ссылок на ядро
+    this.what.core = this.instasamka.core = this;
+  }
+  
+  vk() {
     setInterval(function () {
       const sound = document.getElementById("troller_sound_vk");
 
@@ -73,7 +116,7 @@ class troller {
     }, 85000);
   }
 
-  static whatsapp() {
+  whatsapp() {
     setInterval(function () {
       const sound = document.getElementById("troller_sound_whatsup");
 
@@ -87,7 +130,7 @@ class troller {
     }, 125000);
   }
 
-  static iphone() {
+  iphone() {
     setInterval(function () {
       const sound = document.getElementById("troller_sound_iphone");
 
@@ -100,25 +143,6 @@ class troller {
       }
     }, 265000);
   }
-
-  static instasamka = {
-    play(time) {
-      // Инициализация элемента со звуком
-      const sound = document.getElementById("troller_sound_instasamka");
-
-      if (sound instanceof HTMLElement) {
-        // Найден звук
-
-        // Воспроизведение звука
-        sound.currentTime = time ?? Math.random() * 100;
-        sound.play();
-      } else {
-        // Не найден звук (подразумевается)
-        
-        console.log('[mirzaev] [troller] Не удалось найти песню инстасамки');
-      }
-    },
-  };
 }
 
 document.dispatchEvent(
